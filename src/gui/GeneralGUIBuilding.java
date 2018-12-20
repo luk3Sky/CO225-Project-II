7cc105c433db20a7e150f71f3dcdec74bd9e0798
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-12-20 13:28:03 +0530

    JPanel & some components added

--- src/gui/GeneralGUIBuilding.java
+++ src/gui/GeneralGUIBuilding.java
@@ -7,6 +7,18 @@ public class GeneralGUIBuilding {
 
     public static void main(String[] args){
         JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        frame.getContentPane().add(panel);
+
+        JButton btn = new JButton("Click");
+        panel.add(btn);
+
+        JTextField txtField = new JTextField();
+        txtField.setPreferredSize(new Dimension(200,24));
+        panel.add(txtField);
+
         frame.setSize(new Dimension(500,400));
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
