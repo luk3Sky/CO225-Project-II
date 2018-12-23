c03ad5a406a13dec2489007b4078e8a5c3043fa6
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-12-23 23:34:17 +0530

    chat server gui created

---
+++ src/ExChat/ChatServer.java
@@ -0,0 +1,54 @@
+package ExChat;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ChatServer extends JFrame {
+
+    public static final String TITLE="Chat Server";
+    private static ChatServer instance;
+    private JTextPane console;
+    private JList listUsers;
+
+    public ChatServer(){
+        createView();
+
+        setTitle(TITLE);
+        setSize(500,400);
+        setResizable(true);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public void createView(){
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+
+        panel.setLayout(new BorderLayout());
+
+        console = new JTextPane();
+        console.setEditable(false);
+        JScrollPane consoleScrollPane = new JScrollPane(console);
+        consoleScrollPane.setBorder(BorderFactory.createTitledBorder("Console Output"));
+        panel.add(consoleScrollPane, BorderLayout.CENTER);
+
+        listUsers = new JList();
+        JScrollPane userScrollPane = new JScrollPane(listUsers);
+        userScrollPane.setPreferredSize(new Dimension(200,0));
+        panel.add(userScrollPane, BorderLayout.EAST);
+    }
+    public static ChatServer getInstance() {
+
+        return instance;
+    }
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                instance = new ChatServer();
+                instance.setVisible(true);
+            }
+        });
+    }
+}
